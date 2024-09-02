d18d34df172c823243938848d7811bc2cd34554c
unclejames2610 <udealaebuka@gmail.com>
2024-09-02 12:32:05 +0100

    feat:query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
   return <h1>TanStack Query</h1>;
 }
 
 function wait(duration) {
-  return new Promise();
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
